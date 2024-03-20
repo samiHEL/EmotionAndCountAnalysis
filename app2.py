e81b4aeea13064d5a4c4c0bf6c8e788e3b75bda3
samiHEL <sami.hella.sio@gmail.com>
2024-03-20 12:40:48 +0100

    new readme

--- app2.py
+++ app2.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import torch
 from models import vgg19  # Assurez-vous que models.py est correctement défini et dans votre PATH
 from torchvision import transforms
-
+###### Nous utiliserons deux models Hugging face et un model local #####
 # Chemin vers votre modèle local
 model_path = "model_sh_B.pth"
 device = torch.device('cpu')  # Changez pour 'cuda' si vous utilisez un GPU
@@ -16,7 +16,7 @@ model.to(device)
 model.load_state_dict(torch.load(model_path, map_location=device))
 model.eval()
 
-# Les pipelines pour la légende et la classification des émotions
+# Pipeline pour faire une legende et pour detecter emotions -> importées grâce à transformers
 caption_pipeline = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
 emotion_pipeline = pipeline("image-classification", model="RickyIG/emotion_face_image_classification_v3")
 
@@ -36,22 +36,22 @@ def predict_count(image):
     return int(count)
 
 def generate_caption_emotion_and_people_count(image):
-    # Génération de la légende
+    # Générer une légende 1 ere etape
     caption_result = caption_pipeline(image)
     caption = caption_result[0]["generated_text"]
 
-    # Classification des émotions
+    # Classification des émotions 2 eme etape
     emotion_result = emotion_pipeline(image)
     emotions = ", ".join([f"{res['label']}: {res['score']:.2f}" for res in emotion_result])
 
-    # Comptage des personnes
+    # Comptage des personnes dans la photo 3 eme etaoe
     count = predict_count(image)
 
     # Combinaison des résultats
     combined_result = f"Caption: {caption}\nEmotions: {emotions}\nNumber of People: {count}"
     return combined_result
 
-# URL de l'image à tester
+# URL de l'image à tester en attendanrt version final
 image_url = "https://img.freepik.com/photos-premium/equipe-souriante-fond-transparent_894067-17853.jpg"
 
 # Chargement de l'image
